b17690802dca7d83b4cc1a2d346202134c7ebfb9
LoveLifeEveryday <913710642@qq.com>
2020-06-05 00:04:43 +0800

    feat(TreeFragment):TreeFragment 适配了沉浸式状态栏

--- app/src/main/java/com/xcynice/playxandroid/module/tree/TreeFragment.java
+++ app/src/main/java/com/xcynice/playxandroid/module/tree/TreeFragment.java
@@ -1,8 +1,16 @@
 package com.xcynice.playxandroid.module.tree;
 
+import android.content.res.Configuration;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabLayout;
+import com.gyf.immersionbar.ImmersionBar;
+import com.gyf.immersionbar.components.SimpleImmersionOwner;
+import com.gyf.immersionbar.components.SimpleImmersionProxy;
 import com.xcynice.playxandroid.R;
 import com.xcynice.playxandroid.adapter.CommonViewPagerAdapter;
 import com.xcynice.playxandroid.base.BaseFragment;
@@ -22,13 +30,14 @@ import butterknife.BindView;
 
 
 @SuppressWarnings("rawtypes")
-public class TreeFragment extends BaseFragment {
+public class TreeFragment extends BaseFragment implements SimpleImmersionOwner {
 
 
     @BindView(R.id.tl_tree)
     TabLayout mTlTree;
     @BindView(R.id.vp_tree)
     ViewPager mVpTree;
+    private SimpleImmersionProxy mSimpleImmersionProxy = new SimpleImmersionProxy(this);
 
     @SuppressWarnings("rawtypes")
     @Override
@@ -59,5 +68,43 @@ public class TreeFragment extends BaseFragment {
         mVpTree.setAdapter(commonViewPagerAdapter);
     }
 
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        mSimpleImmersionProxy.setUserVisibleHint(isVisibleToUser);
+    }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mSimpleImmersionProxy.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mSimpleImmersionProxy.onDestroy();
+    }
+
+    @Override
+    public void onHiddenChanged(boolean hidden) {
+        super.onHiddenChanged(hidden);
+        mSimpleImmersionProxy.onHiddenChanged(hidden);
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mSimpleImmersionProxy.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public void initImmersionBar() {
+        ImmersionBar.with(this).titleBar(mTlTree).init();
+    }
+
+    @Override
+    public boolean immersionBarEnabled() {
+        return true;
+    }
 }
